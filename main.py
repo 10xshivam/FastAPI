0a15f5af99968829e18a01a9e9d43a7640408c8b
Shivam <kumarshivam3788@gmail.com>
2025-08-27 13:48:53 +0530

    Implement patient update endpoint and modify patient data handling

--- main.py
+++ main.py
@@ -100,3 +100,34 @@ def create_patient(patient: Patient):
     saveData(data)
 
     return JSONResponse(status_code=201, content={"message": "Patient created successfully.", "patient": data[patient.id]})
+
+@app.put("/edit/{patient_id}")
+def update_patient(patient_id:str,patient_update:PatientUpdate):
+    # Load existing data
+    data = loadData()
+
+    # Check if the patient exists
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found.")
+
+    # Update patient data
+    existing_patient_info = data[patient_id]
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+    
+    for key, value in updated_patient_info.items(): # jo value set krke bheji gyi hai vhi ayegi
+        existing_patient_info[key] = value
+    
+    # existing_patient_info -> pydantic obejct -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydatic_object = Patient(**existing_patient_info)
+    
+    # pydantic object -> dict
+    existing_patient_info = patient_pydatic_object.model_dump(exclude=['id'])
+    
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+    
+    # save
+    saveData(data)
+
+    return JSONResponse(status_code=200, content={"message": "Patient updated successfully."})
